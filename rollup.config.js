76308d6c4a788118378990860c84c79cd209c48d
John Haugeland <stonecypher@gmail.com>
2018-03-31 22:10:39 -0700

    dotfiles and config

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs    from 'rollup-plugin-commonjs';
+
+const pkg = require('./package');
+
+export default {
+
+  input     : 'src/js/fslp.js',
+//moduleId  : pkg.name,
+
+  output    : {
+    file      : 'build/fslp.js',
+    format    : 'cjs',
+    name      : 'fslp',
+    sourcemap : true,
+  },
+
+  plugins   : [
+
+    commonjs({
+      namedExports: {
+//      'node_modules/react/index.js'     : ['createElement', 'Component'],
+//      'node_modules/react-dom/index.js' : ['render'],
+      },
+    }),
+
+    nodeResolve({
+      module: true,
+      jsnext: true,
+      browser: true,
+      extensions: [ '.js', '.json', '.jsx', '.ts', '.tsx' ],
+//    extensions: [ '.js', '.json', '.ts' ],
+      preferBuiltins: false
+    })
+
+  ]
+}
